b30ffcc329670a8fd58d0e4ce8620b1c490e7e6b
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-10-23 15:46:21 +1100

    Test reading ints from stdin

--- examples/minimal.c
+++ examples/minimal.c
@@ -4,26 +4,18 @@ void output_int(int val) {
   __asm("str r1, FF");
 }
 
+int input_int(void) {
+  int res;
+  __asm("lod r1, FF" : "=r" (res));
+  return res;
+}
+
 int test_not(int a) {
   return ~a;
 }
 
-int test_not_redundant(void) {
-  return ~0xFFFF;
-}
-
-int test_not_halfword(void) {
-  return ~1;
-}
-
-int test_not_word(void) {
-  return ~256;
-}
-
 int main(void) {
-  output_int(test_not(1));
-  output_int(test_not_redundant());
-  output_int(test_not_halfword());
+  int val = input_int();
 
-  return test_not_word();
+  return test_not(val);
 }
